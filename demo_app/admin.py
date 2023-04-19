250aa72d8d99376f605d6186789dfab3216cffbd
godd0t <lirrishala@gmail.com>
2023-04-19 21:20:31 +0200

    feat: add schemas and api

--- demo_app/admin.py
+++ demo_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import Book, Tag
 
 admin.site.register(Book)
--- demo_app/api.py
+++ demo_app/api.py
@@ -1,29 +1,26 @@
-from typing import List
-from uuid import UUID
+from typing import List, Tuple
+
 from ninja.params import Body
 from ninja_extra import api_controller, route
 
 from demo_app.models import Book, Tag
-from schemas import BookSchema, BookCreateSchema
+from demo_app.schemas import BookCreateSchema, BookSchema
 
 
 @api_controller("", tags=["Books"])
 class BookController:
-
     @route.get("books/", response={200: List[BookSchema]})
     async def get_books(
-            self,
+        self,
     ) -> List[Book]:
-        book = [book async for book in Book.objects.all()]
-        print(book)
         return [book async for book in Book.objects.all()]
 
-    @route.post("books/", response={201: BookSchema})
+    @route.post("books/", response={201: BookSchema})  # <-- New route
     async def create_book(
-            self,
-            payload: BookCreateSchema = Body(..., description="Book payload"),
-    ) -> Book:
+        self,
+        payload: BookCreateSchema = Body(..., description="Book payload"),
+    ) -> Tuple[int, Book]:
         book = await Book.objects.acreate(title=payload.title)
         tags = [await Tag.objects.acreate(name=name) for name in payload.tags]
         await book.tags.aset(tags)
-        return book
+        return 201, book
